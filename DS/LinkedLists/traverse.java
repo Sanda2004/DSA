687d3b04df504217275fcf85120153982b5c828e
Sanda2004 <sandaliabeykoon2004@gmail.com>
2025-07-13 07:14:53 +0530

    Update traverse.java

--- DS/LinkedLists/traverse.java
+++ DS/LinkedLists/traverse.java
@@ -1,1 +1,38 @@
 
+package list;
+
+/**
+ *
+ * @author Sandali
+ */
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int new_data){
+        this.data = new_data;
+        this.next =null;
+    }
+}
+public class traversal {
+    
+    public static void traverseList(Node current){
+        
+        while(current!=null){
+            System.out.println(current.data+"");
+            
+            current = current.next;                 
+        }
+    }
+    
+    public static void main(String[] args) {
+        Node current = new Node(10);
+        current.next = new Node(20);
+        current.next.next = new Node(30);
+        
+        
+        traverseList(current);
+    }
+}
+
