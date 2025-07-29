8df9b39234fb55f800288166713535cf782a372d
Sanda2004 <sandaliabeykoon2004@gmail.com>
2025-07-30 05:22:45 +0530

    Update _hashsetset.java

--- DS/HashSetSet/_hashsetset.java
+++ DS/HashSetSet/_hashsetset.java
@@ -1,1 +1,28 @@
 
+package dsa;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author Sandali
+ */
+public class _HashSet {
+ 
+    public static void main(String[] args) {
+        HashSet<String> fruits = new HashSet<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        fruits.add("Apple"); 
+
+        System.out.println("HashSet: " + fruits);
+
+        fruits.remove("Banana");
+        System.out.println("After removing Banana: " + fruits);
+
+        System.out.println("Contains Mango? " + fruits.contains("Mango"));
+    }
+}
+
