dfb0fe66ee0dd52776ef2f7cf29779423e0747d6
Sanda2004 <sandaliabeykoon2004@gmail.com>
2025-07-30 05:18:36 +0530

    Update PriorityQueueQueue.java

--- DS/ProrityQueueQueue/PriorityQueueQueue.java
+++ DS/ProrityQueueQueue/PriorityQueueQueue.java
@@ -1,1 +1,16 @@
-
+public class priorityqueue {
+    public static void main(String[] args) {
+        Queue<Double> queue = new PriorityQueue<>();
+        
+        queue.add(2.4);
+        queue.add(5.2);
+        queue.add(4.0);
+        
+        System.out.println("PriorityQueue: " + queue);
+        
+        while(!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
+    }
+    
+}
